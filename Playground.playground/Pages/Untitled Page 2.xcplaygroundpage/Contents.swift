4c109d72637f118ccdf663c90b569c8fb7fc565d
kevintriestocode <kwangsan@gmail.com>
2017-06-27 14:28:40 -0400

    j

--- Playground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -22,19 +22,28 @@ func divisibleByTwo(_ number: Int) -> Bool {
     return number % 2 == 0
 }
 
-let evenNumbers = filterInts(numbers, { (_ number: Int) -> Bool in return number % 2 == 0 })
-/*
-Type Inference In Closures
-let evenNumbers = filterInts(numbers, { number in return number % 2 == 0 })
-let evenNumbers = filterInts(numbers, { number in number % 2 == 0 })
-Implicit Arguments In Closures
-let evenNumbers = filterInts(numbers, { $0 % 2 == 0 })
-Trailing Closures
+//let evenNumbers = filterInts(numbers, { (_ number: Int) -> Bool in return number % 2 == 0 })
+//Type Inference In Closures
+//let evenNumbers = filterInts(numbers, { number in return number % 2 == 0 })
+//let evenNumbers = filterInts(numbers, { number in number % 2 == 0 })
+//Implicit Arguments In Closures
+//let evenNumbers = filterInts(numbers, { $0 % 2 == 0 })
+//Trailing Closures
 let evenNumbers = filterInts(numbers) { $0 % 2 == 0 }
-*/ //:29:01 
+
+
+//:29:01
 print(evenNumbers)
 
-
+let evenDigitSums = filterInts(numbers) { number in
+    var sum = 0, number = number
+    while number > 0 {
+        //... calculate sum of digits
+        //... 82 is 8 + 2 = 10, which is even
+    }
+    return sum % 2 == 0
+}
+print(evenDigitSums)
 
 
 
